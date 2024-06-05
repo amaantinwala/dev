897ecce5aa802275336a4aab742f009fdbf9dab8
amaantinwala <50973896+amaantinwala@users.noreply.github.com>
2024-06-05 20:13:02 +0530

    objects

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const course = {
+    courseTitle: "Javascript in hindi",
+    coursePrice: "405.2",
+    courseInstructor: "Amaan",
+    courseBuyers: ["Amaan","Faizan","Kumail"]
+};
+
+// Object Destructuring
+
+const {courseInstructor} = course;
+
+console.log(courseInstructor);
